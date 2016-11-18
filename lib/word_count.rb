54e591149065e6c326c0a6671fc4d3b751746a24
Michael E. Andrade <andrade.michael.e@gmail.com>
2016-11-18 11:22:24 -0800

    add input check for empty strings and return 0 if no match

--- lib/word_count.rb
+++ lib/word_count.rb
@@ -1,8 +1,11 @@
 class String
   define_method(:word_count) do |search_text|
     if self == "" || search_text == ""
-      return "empty"
+      break
+    elsif self == search_text
+      1
     else
+      0
     end
   end
 end
--- spec/word_count_spec.rb
+++ spec/word_count_spec.rb
@@ -3,7 +3,12 @@ require('rspec')
 require('word_count')
 
 describe("Word Count will accept two strings and count how many times the first string occurs in the second string.") do
-  it("Returns an error message if either field is empty or contains no word characters.") do
-    expect("".word_count("")).to(eq("empty"))
+  it("Returns nil if either field is empty or contains no word characters.") do
+    expect("".word_count("")).to(eq(nil))
   end
+  it("Returns 1 if the search term and the search text match and 0 otherwise") do
+    expect("dog".word_count("dog")).to(eq(1))
+    expect("dog".word_count("cat")).to(eq(0))
+  end
+
 end
